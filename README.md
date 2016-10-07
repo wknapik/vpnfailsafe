626757b15d764e75acb3e3a40e3d5b2b12c1a00b
Wojciech Knapik <wmknapik@gmail.com>
2016-10-07 15:57:41 +0200

    Added the script, added example.conf, updated README.

--- README.md
+++ README.md
@@ -1,1 +1,103 @@
-# vpnfailsafe
+# What is vpnailsafe ?
+
+`vpnfailsafe` prevents a VPN user's non-VPN IP from being exposed on the
+internet, both while the VPN connection is active and when it goes down.
+
+`vpnfailsafe` doesn't affect traffic to/from private networks, or disrupt existing
+firewall rules beyond its intended function.
+
+# How does it work ?
+
+`vpnfailsafe` ensures that all traffic to/from the internet goes through the VPN.
+It is meant to be executed by OpenVPN when the tunnel is established (--up), or
+torn down (--down). 
+
+On --up:
+* All configured VPN server domains are resolved and saved in /etc/hosts (once,
+  querying the originally configured DNS server from the public IP and later,
+  periodically, only using the VPN and the DNS pushed by the server).
+* Routes are set up, so that all traffic to the internet goes over the tunnel.
+  The original default route is preserved and two more specific ones are added
+  (mimicking --redirect-gateway def1) + a route to the currently used VPN
+  server is added.
+* /etc/resolv.conf is updated, so only the DNS pushed by the VPN server is used.
+* iptables rules are inserted at the beginning of INPUT, OUTPUT and FORWARD
+  chains to ensure that the only traffic to/from the internet is between the
+  VPN client and the VPN server.
+
+On --down:
+* Periodic /etc/hosts updates are stopped, but the entries for VPN servers are
+  kept, so the VPN connection can be re-established without querying DNS
+  servers outside the VPN.
+* Previously added routes are removed.
+* Previous /etc/resolv.conf is restored.
+* Firewall rules remain in place, allowing only the re-establishment of the vpn
+  tunnel.
+
+# How do I install/use it ?
+
+Save vpnfailsafe&#46;sh in /etc/openvpn, make it executable and add the
+following lines to /etc/openvpn/<your_provider>.conf:
+
+```
+up /etc/openvpn/vpnfailsafe.sh
+down /etc/openvpn/vpnfailsafe.sh
+```
+
+That's it.
+
+A complete example is included as example.conf.
+
+If you want to use --user/--group to drop root priveleges, or otherwise run as
+an unprivileged user, prepare for an uphill battle. OpenVPN will not make it
+easy and the changes to get full functionality as non-root are likely to be
+invasive. Perhaps a working example will be added in the future.
+
+# What are the requirements/assumptions/limitations ?
+
+Beyond OpenVPN, the requirements are intentionally kept minimal. The list of
+required packages is at the top of the script. One assumption is that the VPN
+server will push at least one DNS to the client.
+
+`vpnfailsafe` was tested on Arch Linux, with a tun-device-based VPN (IP
+encapsulation).
+
+There is no ipv6 support.
+
+# How do I restore my system to the state from before running vpnfailsafe ?
+
+`vpnfailsafe` will revert all changes when the tunnel is closed, except for the
+firewall rules. You can restore those using the init script that set the
+iptables rules on boot, or by otherwise removing the VPNFAILSAFE_INPUT,
+VPNFAILSAFE_OUTPUT and VPNFAILSAFE_FORWARD chains.
+
+If you're not going to use the VPN for a while, the entries in /etc/hosts for
+the VPN servers might get stale and require removal.
+
+# Will vpnfailsafe protect me against DNS leaks ?
+
+Yes. See "How does it work ?" for more details.
+
+That being said, if your life, job, or whatever you care about depend on your
+IP not leaking, consider that this script has been tested by one person with
+one VPN provider and a single configuration. YMMV.
+
+# Will vpnfailsafe protect me against all forms of IP leaks ?
+
+No. Application level leaks can still happen, via protocols like WebRTC, or
+BitTorrent. The user can also announce their identity to the world and no
+script will stop them.
+
+# Do I still need to configure a firewall ?
+
+Yes. `vpnfailsafe` limits what kind of traffic is allowed, but only to achieve
+its goals. Otherwise everything is passed through to pre-existing firewall
+rules.
+
+# Aren't there already scripts that do all that ?
+
+One would think so, but then one would be wrong.
+
+What is out there are mostly "applications", with non-optional GUIs and
+thousands of lines of code behind them, often VPN-provider specific.
+
---
+++ vpnfailsafe.sh
@@ -0,0 +1,184 @@
+#!/usr/bin/env bash
+# Requirements: bash, coreutils, glibc, grep, iproute2, iptables, procps-ng, openresolv, sed.
+
+set -eEuo pipefail
+
+readonly prog="$(basename "$0")"
+readonly private_nets="10.0.0.0/8,172.16.0.0/12,192.168.0.0/16"
+readonly dev
+readonly ${!foreign_option_*}
+readonly ifconfig_local
+readonly ifconfig_netmask
+readonly ${!proto_*}
+readonly ${!remote_*}
+readonly ${!remote_port_*}
+readonly route_net_gateway
+readonly route_vpn_gateway
+readonly script_type
+readonly trusted_ip
+
+# $@ := ""
+error() {
+    exit 1
+}
+
+# $@ := "up" | "down"
+update_hosts() {
+    pkill -P1 -f "$prog" || true
+    if [[ "$@" == up ]]; then
+        uh() {
+            local -r beg="# VPNFAILSAFE BEGIN" end="# VPNFAILSAFE END"
+            local -ar remotes=($(env|grep -oP 'remote_[0-9]+=.*'|cut -d= -f2))
+            local -r maybe_inplace="$([[ "$PPID" -eq 1 ]] && echo -i || echo)"
+            {
+                sed $maybe_inplace /etc/hosts -e "/^$beg/,/^$end/d" 
+                echo "$beg"
+                getent hosts "${remotes[@]}"|grep -v :
+                echo "$end"
+            } >/etc/hosts.vpnfailsafe
+            chmod --reference=/etc/hosts /etc/hosts.vpnfailsafe
+            mv /etc/hosts.vpnfailsafe /etc/hosts
+        }
+        uh
+        while true; do sleep 5m; uh; done& 
+    fi
+}
+
+# $@ := "up" | "down"
+update_routes() {
+    if [[ "$@" == up ]]; then
+        if [[ -z $(ip route show "$trusted_ip") ]]; then
+            ip route add "$trusted_ip/32" via "$route_net_gateway"
+        fi
+        for net in 0.0.0.0/1 128.0.0.0/1; do
+            [[ -z $(ip route show "$net") ]] && ip route add "$net" via "$route_vpn_gateway"
+        done
+    elif [[ "$@" == down ]]; then
+        for route in "$trusted_ip/32" 0.0.0.0/1 128.0.0.0/1; do
+            [[ -n $(ip route show "$route") ]] && ip route del "$route"
+        done
+        if [[ -n $(ip addr show dev "$dev" 2>/dev/null) ]]; then
+            ip addr del "$ifconfig_local/$ifconfig_netmask" dev "$dev"
+        fi
+    else
+        error
+    fi
+}
+
+# $@ := "up" | "down"
+update_resolv() {
+    case "$@" in
+        up) local domains="" ns=""
+            for opt in "${!foreign_option_*}"; do
+                case "${!opt}" in
+                    dhcp-option\ DOMAIN*) domains+="${!opt##* }";;
+                    dhcp-option\ DNS\ *) ns+=" ${!opt##* }";;
+                    *) ;;
+                esac
+            done
+            echo -e "${domains/ /search }\n${ns// /$'\n'nameserver }"|resolvconf -a "$dev";;
+        down) resolvconf -d "$dev" 2>/dev/null || true;;
+        *) error;;
+    esac
+}
+
+# $@ := ""
+update_firewall() {
+    local -r public_nic="$(ip route show "$trusted_ip"|cut -d' ' -f5)"
+
+    # $@ := "INPUT" | "OUTPUT" | "FORWARD"
+    insert_chain() {
+        if iptables -C "$*" -j "VPNFAILSAFE_$*" 2>/dev/null; then
+            iptables -D "$*" -j "VPNFAILSAFE_$*"
+            for opt in F X; do
+                iptables -"$opt" "VPNFAILSAFE_$*"
+            done
+        fi
+        iptables -N "VPNFAILSAFE_$*"
+        iptables -I "$*" -j "VPNFAILSAFE_$*"
+    }
+
+    # $@ := "INPUT" | "OUTPUT"
+    accept_remotes() {
+        case "$@" in
+            INPUT)  local -r sd=s states=""   io=i;;
+            OUTPUT) local -r sd=d states=NEW, io=o;;
+            *) error;;
+        esac
+        for remote in ${!remote_*}; do
+            if [[ "$remote" =~ ^remote_[0-9]+$ ]]; then
+                local port="remote_port_${remote##*_}"
+                local proto="proto_${remote##*_}"
+                iptables -A "VPNFAILSAFE_$*" -p "${!proto}" -"$sd" "${!remote}" --"$sd"port "${!port}" \
+                    -m conntrack --ctstate "$states"RELATED,ESTABLISHED -"$io" "$public_nic" -j ACCEPT
+            fi
+        done
+    }
+
+    # $@ := "INPUT" | "OUTPUT" | "FORWARD"
+    pass_private_nets() { 
+        case "$@" in
+            INPUT) local -r sd=s io=i;;
+            OUTPUT|FORWARD) local -r sd=d io=o;;
+            *) error;;
+        esac
+        if [[ "$@" != FORWARD ]]; then
+            iptables -A "VPNFAILSAFE_$*" -"$sd" "$ifconfig_local/$ifconfig_netmask" -"$io" "$dev" -j RETURN
+        fi
+        iptables -A "VPNFAILSAFE_$*" -"$sd" "$private_nets" ! -"$io" "$dev" -j RETURN
+    }
+
+    # $@ := "INPUT" | "OUTPUT" | "FORWARD"
+    pass_vpn() {
+        case "$@" in
+            INPUT)
+                iptables -A "VPNFAILSAFE_$*" -s "$private_nets" -i "$dev" -j DROP
+                local -r io=i;;
+            OUTPUT|FORWARD)
+                local -r io=o;;
+            *) error;;
+        esac
+        iptables -A "VPNFAILSAFE_$*" -"$io" "$dev" -j RETURN
+    }
+
+    # $@ := "INPUT" | "OUTPUT" | "FORWARD"
+    drop_other() {
+        iptables -A "VPNFAILSAFE_$*" -j DROP
+    }
+
+    for chain in INPUT OUTPUT FORWARD; do
+        insert_chain "$chain"
+        [[ "$chain" != FORWARD ]] && accept_remotes "$chain"
+        pass_private_nets "$chain"
+        pass_vpn "$chain"
+        drop_other "$chain"
+    done
+}
+
+# $@ := ""
+cleanup() {
+    update_resolv down
+    update_routes down
+    update_hosts down
+}
+trap cleanup INT TERM
+
+# $@ := line_number exit_code
+err_msg() {
+    echo "$0:$1: \`$(sed -n "$1,+0{s/^\s*//;p}" "$0")\` returned $2" >&2
+    cleanup
+}
+trap 'err_msg "$LINENO" "$?"' ERR
+
+# $@ := ""
+main() {
+    local -r st="${script_type:-down}"
+    update_hosts "$st"
+    update_routes "$st"
+    update_resolv "$st"
+    if [[ "${st}" == up ]]; then
+        update_firewall
+    fi
+}
+
+main
