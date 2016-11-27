87cb05a29263f0ed01bb13c3efbde0a5b63310a0
Wojciech Knapik <wmknapik@gmail.com>
2016-11-27 04:33:19 +0100

    Added support for remotes defined as any mix of domains and IPs.

--- vpnfailsafe.sh
+++ vpnfailsafe.sh
@@ -15,14 +15,20 @@ readonly route_vpn_gateway
 readonly script_type
 readonly trusted_ip
 readonly trusted_port
+readonly untrusted_ip
+readonly untrusted_port
 
 readonly prog="$(basename "$0")"
 readonly private_nets="127.0.0.0/8,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16"
 readonly -a remotes=($(env|grep -oP 'remote_[0-9]+=.*'|sort -n|cut -d= -f2))
+readonly -a cnf_remote_domains=(${remotes[@]%%*[0-9]})
+readonly -a cnf_remote_ips=(${remotes[@]##*[!0-9.]*})
+readonly cur_remote_ip="${trusted_ip:-$untrusted_ip}"
+readonly cur_port="${trusted_port:-$untrusted_port}"
 
 # $@ := ""
 update_hosts() {
-    if remote_entries="$(getent -s dns hosts "${remotes[@]}"|grep -v :)"; then
+    if remote_entries="$(getent -s dns hosts "${cnf_remote_domains[@]}"|grep -v :)"; then
         local -r beg="# VPNFAILSAFE BEGIN" end="# VPNFAILSAFE END"
         {
             sed -e "/^$beg/,/^$end/d" /etc/hosts
@@ -35,9 +41,10 @@ update_hosts() {
 
 # $@ := "up" | "down"
 update_routes() {
-    local -ar remote_ips=($(getent -s files hosts "${remotes[@]}"|cut -d' ' -f1))
+    local -ar resolved_ips=($(getent -s files hosts "${cnf_remote_domains[@]:-!NONE!}"|cut -d' ' -f1 || true))
+    local -ar remote_ips=("${resolved_ips[@]}" "${cnf_remote_ips[@]}")
     if [[ $@ == up ]]; then
-        for remote_ip in "$trusted_ip" "${remote_ips[@]}"; do
+        for remote_ip in "$cur_remote_ip" "${remote_ips[@]}"; do
             if [[ -z $(ip route show "$remote_ip") ]]; then
                 ip route add "$remote_ip" via "$route_net_gateway"
             fi
@@ -48,7 +55,7 @@ update_routes() {
             fi
         done
     elif [[ $@ == down ]]; then
-        for route in "$trusted_ip" "${remote_ips[@]}" 0.0.0.0/1 128.0.0.0/1; do
+        for route in "$cur_remote_ip" "${remote_ips[@]}" 0.0.0.0/1 128.0.0.0/1; do
             if [[ -n $(ip route show "$route") ]]; then
                 ip route del "$route"
             fi
@@ -92,18 +99,18 @@ update_firewall() {
             INPUT)  local -r sd=s states=""   io=i;;
             OUTPUT) local -r sd=d states=NEW, io=o;;
         esac
-        local -r public_nic="$(ip route show "$trusted_ip"|cut -d' ' -f5)"
+        local -r public_nic="$(ip route show "$cur_remote_ip"|cut -d' ' -f5)"
         local -ar suf=(-m conntrack --ctstate "$states"RELATED,ESTABLISHED -"$io" "${public_nic:?}" -j ACCEPT)
-        if ! _trusted_ip_in_remote_ips="$(getent -s files hosts "$trusted_ip")"; then
-            for p in tcp udp; do
-                iptables -A "VPNFAILSAFE_$*" -p "$p" -"$sd" "$trusted_ip" --"$sd"port "${trusted_port}" "${suf[@]}"
-            done
-        fi
         for ((i=1; i <= ${#remotes[*]}; ++i)); do
             local port="remote_port_$i"
             local proto="proto_$i"
             iptables -A "VPNFAILSAFE_$*" -p "${!proto%-client}" -"$sd" "${remotes[i-1]}" --"$sd"port "${!port}" "${suf[@]}"
         done
+        if ! iptables -S|grep -q -- "^-A VPNFAILSAFE_$* .*-$sd $cur_remote_ip/32 .*-j ACCEPT$"; then
+            for p in tcp udp; do
+                iptables -A "VPNFAILSAFE_$*" -p "$p" -"$sd" "$cur_remote_ip" --"$sd"port "${cur_port}" "${suf[@]}"
+            done
+        fi
     }
 
     # $@ := "INPUT" | "OUTPUT" | "FORWARD"
